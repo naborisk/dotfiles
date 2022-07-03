3961bd7e2d1673e514f2722477eee1525a045ac4
Naborisk <naborisk@gmail.com>
2022-07-04 02:27:25 +0900

    change install .vimrc to curl

--- install-vim.sh
+++ install-vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp .vimrc ~/.vimrc
+curl https://raw.githubusercontent.com/naborisk/dotfiles/main/.vimrc > ~/.vimrc
 
 # install dein.vim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s ~/.vim/bundles
