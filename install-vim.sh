b6d87d96a7ac82364c4b212a0f1b979c860a47b9
Naborisk <naborisk@gmail.com>
2022-07-05 15:51:56 +0900

    fix add vim theme

--- install-vim.sh
+++ install-vim.sh
@@ -5,7 +5,7 @@ curl https://raw.githubusercontent.com/naborisk/dotfiles/main/.vimrc > ~/.vimrc
 
 # install darcula theme
 mkdir -p ~/.vim/colors
-curl https://raw.githubusercontent.com/blueshirts/darcula/master/colors/darcula.vim > ~/.vim/colors
+curl https://raw.githubusercontent.com/blueshirts/darcula/master/colors/darcula.vim > ~/.vim/colors/darcula.vim
 
 # install dein.vim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s ~/.vim/bundles
