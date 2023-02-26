8066bea82949e45bf12e6027a0a55ad81c6dc2b7
Naborisk <naborisk@gmail.com>
2023-02-27 02:51:44 +0900

    update .tmux.conf & add colorblind to nightfox

---
+++ nvim/after/plugin/nightfox.lua
@@ -0,0 +1,12 @@
+require'nightfox'.setup{
+  options = {
+    colorblind = {
+      enable = true,
+      severity = {
+        deutan = 1,
+        protan = 0.7,
+        tritan = 0.2
+      }
+    }
+  }
+}
