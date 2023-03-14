58903b223780d7d10ddd0fb57d792d372ab90bc9
Naborisk <naborisk@gmail.com>
2023-03-15 04:46:05 +0900

    WIP lsp config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -22,6 +22,31 @@ lsp.configure('lua_ls', {
   }
 })
 
+vim.diagnostic.config({
+  -- virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
+lsp.configure('emmet_ls', {
+  filetypes = {'html', 'markdown', 'javascipt'}
+})
+
+-- local pid = vim.fn.getpid()
+-- local omnisharp_bin = "/Users/naborisk/.local/share/nvim/mason/packages/omnisharp-mono/run"
+-- local omnisharp_binn = "/.local/share/nvim/mason/packages/omnisharp-mono/run"
+--
+-- lsp.configure('omnisharp_mono', {
+--   cmd = {omnisharp_bin, "--languageserver", "--hostPID", pid}
+-- })
+
+-- require'lspconfig'.omnisharp.setup {
+--  cmd = {omnisharp_bin, "--languageserver", "--hostPID", pid}
+-- }
+
 lsp.set_preferences({
   sign_icons = {
     error = '',
