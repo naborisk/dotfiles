135f09f4c4caf325c05605b0d73ec06c25e094c6
Naborisk <github@naborisk.com>
2023-04-15 14:07:22 +0900

    add <C-p> shortcut for search (similar to notion)

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,6 +3,7 @@ local actions = require('telescope.actions')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 
 -- ripgrep required, grep string in files
 vim.keymap.set('n', '<leader>fr', function()
