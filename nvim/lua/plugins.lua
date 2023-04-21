1d61993ee33ce3e5db09b7518f9392c33bc36994
Naborisk <github@naborisk.com>
2023-04-22 01:34:02 +0900

    clean up

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,12 +1,12 @@
 -- bootstrapping lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
@@ -17,21 +17,21 @@ require'lazy'.setup{
     'VonHeikemen/lsp-zero.nvim',
     dependencies = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      'neovim/nvim-lspconfig',             -- Required
+      'williamboman/mason.nvim',           -- Optional
+      'williamboman/mason-lspconfig.nvim', -- Optional
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+      'hrsh7th/nvim-cmp',         -- Required
+      'hrsh7th/cmp-nvim-lsp',     -- Required
+      'hrsh7th/cmp-buffer',       -- Optional
+      'hrsh7th/cmp-path',         -- Optional
+      'saadparwaiz1/cmp_luasnip', -- Optional
+      'hrsh7th/cmp-nvim-lua',     -- Optional
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
+      'L3MON4D3/LuaSnip',             -- Required
+      'rafamadriz/friendly-snippets', -- Optional
     }
   },
 
@@ -58,7 +58,7 @@ require'lazy'.setup{
     }
   },
 
-  {'folke/which-key.nvim'},
+  'folke/which-key.nvim',
 
   -- Fuzzy finder
   {
