cd12f4092c5e6dfcd8d591538a06f0d14e078ec6
Naborisk <github@naborisk.com>
2023-04-30 11:44:51 +0900

    separate plugin config into files

---
+++ nvim/after/plugin/nvim-lspconfig.lua
@@ -0,0 +1,78 @@
+local lsp = require('lsp-zero')
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
+end)
+
+-- always show icon column
+vim.o.signcolumn = 'yes'
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'tsserver',
+  'lua_ls',
+  'emmet_ls'
+})
+
+lsp.configure('omnisharp', {
+  settings = {
+    omnisharp = {
+      useModernNet = false,
+      monoPath = '/Library/Frameworks/Mono.framework/Versions/Current/',
+    }
+  }
+})
+
+-- lsp.configure('lua_ls', {
+--   settings = {
+--     Lua = {
+--       diagnostics = {
+--         globals = {
+--           'vim', 'jit'
+--         }
+--       }
+--     }
+--   }
+-- })
+
+vim.diagnostic.config({
+  virtual_text = false,   -- show text after diagnostics
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
+lsp.configure('emmet_ls', {
+  filetypes = { 'html', 'markdown', 'javascriptreact', 'typescriptreact', 'vue', 'astro', 'css', 'sass', 'scss', 'less' },
+  init_options = {
+    html = {
+      options = {
+
+      }
+    }
+  }
+})
+
+-- Show diagnostics text on cursor hold
+local lspGroup = vim.api.nvim_create_augroup('Lsp', { clear = true })
+
+vim.api.nvim_create_autocmd('CursorHold', {
+  command = 'lua vim.diagnostic.open_float()',
+  group = lspGroup
+})
+
+lsp.set_preferences({
+  sign_icons = {
+    error = '',
+    warn = '',
+    hint = '',
+    info = ''
+  }
+})
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
