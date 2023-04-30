1789eb1b220782155b42aba178a0e284fcf627fe
Naborisk <github@naborisk.com>
2023-04-30 13:21:41 +0900

    fix lsp (set lazy=false)

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -36,7 +36,7 @@ require 'lazy'.setup {
   {
     'neovim/nvim-lspconfig',
     cmd = 'LspInfo',
-    -- lazy = false,
+    lazy = false,
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       { 'hrsh7th/cmp-nvim-lsp' },
