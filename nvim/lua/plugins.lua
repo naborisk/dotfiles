b9faed4054b14ec4e64b8faf0d79dfe796ffdb16
Naborisk <5954030+naborisk@users.noreply.github.com>
2024-11-22 10:30:40 +0900

    fix avante breaking change

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -182,27 +182,10 @@ require('lazy').setup({
       provider = 'ollama',
       vendors = {
         ollama = {
-          ['local'] = true,
+          __inherited_from = 'openai',
+          api_key_name = "",
           endpoint = os.getenv 'AVANTE_OLLAMA_ENDPOINT' or '127.0.0.1:11434/v1',
           model = os.getenv 'AVANTE_OLLAMA_MODEL' or 'llama3.1',
-          parse_curl_args = function(opts, code_opts)
-            return {
-              url = opts.endpoint .. '/chat/completions',
-              headers = {
-                ['Accept'] = 'application/json',
-                ['Content-Type'] = 'application/json',
-              },
-              body = {
-                model = opts.model,
-                messages = require('avante.providers').copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
-                max_tokens = 2048,
-                stream = true,
-              },
-            }
-          end,
-          parse_response_data = function(data_stream, event_state, opts)
-            require('avante.providers').openai.parse_response(data_stream, event_state, opts)
-          end,
         },
       },
     },
