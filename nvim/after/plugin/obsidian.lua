796dc19f225538133f1af0bec2ce1aad87b6d99c
Naborisk <github@naborisk.com>
2025-03-29 18:56:21 +0900

    disable obsidian.nvim

--- nvim/after/plugin/obsidian.lua
+++ nvim/after/plugin/obsidian.lua
@@ -1,20 +1,20 @@
-require('obsidian').setup {
-  workspaces = {
-    {
-      name = 'personal',
-      path = '~/obsidian',
-    },
-  },
-  daily_notes = {
-    folder = '01-daily-notes',
-    date_format = '%Y-%m-%d',
-    template = '02-templates/daily',
-  },
-  disable_frontmatter = true,
-  templates = {
-    folder = '02-templates',
-  },
-  follow_url_func = function(url)
-    vim.fn.jobstart { 'open', url }
-  end,
-}
+-- require('obsidian').setup {
+--   workspaces = {
+--     {
+--       name = 'personal',
+--       path = '~/obsidian',
+--     },
+--   },
+--   daily_notes = {
+--     folder = '01-daily-notes',
+--     date_format = '%Y-%m-%d',
+--     template = '02-templates/daily',
+--   },
+--   disable_frontmatter = true,
+--   templates = {
+--     folder = '02-templates',
+--   },
+--   follow_url_func = function(url)
+--     vim.fn.jobstart { 'open', url }
+--   end,
+-- }
