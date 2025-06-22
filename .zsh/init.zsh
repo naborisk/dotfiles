02e9d8db2cc6f11c9a1c0496e049b57bca7a958c
Naborisk <github@naborisk.com>
2025-06-22 13:43:46 +0900

    fix mise init path

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -1,3 +1,3 @@
 command -v starship > /dev/null && eval "$(starship init zsh)"
 command -v zoxide > /dev/null && eval "$(zoxide init --cmd cd zsh)"
-command -v mise > /dev/null && eval "$(mise activate zsh)"
+command -v mise > /dev/null && eval "$($HOME/.local/bin/mise activate zsh)"
