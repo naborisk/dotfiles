5b478b291cd7ff9f5df71ff06b2e310b5ed78c7a
Naborisk <5954030+naborisk@users.noreply.github.com>
2025-06-23 08:38:13 +0900

    Update init.zsh

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -1,3 +1,6 @@
 command -v starship > /dev/null && eval "$(starship init zsh)"
 command -v zoxide > /dev/null && eval "$(zoxide init --cmd cd zsh)"
+# for local installation of mise
 test -e $HOME/.local/bin/mise > /dev/null && eval "$($HOME/.local/bin/mise activate zsh)"
+# for brew installation of mise
+command -v mise > /dev/null && eval "$(mise activate zsh)"
